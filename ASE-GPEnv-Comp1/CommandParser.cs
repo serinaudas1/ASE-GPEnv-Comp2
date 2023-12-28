0b894388fab7ea7b8714b694759a4f2099c63206
serinaudas <serinaudas@gmail.com>
2023-12-29 01:37:57 +0500

    [3: Command Parser Class]- Added Command Parser Skeleton

---
+++ ASE-GPEnv-Comp1/CommandParser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_GPEnv_Comp1
+{
+    class CommandParser
+    {
+
+        public void executeOneCommand(String line) {
+
+        }
+
+        public void executePrograme()
+        {
+
+        }
+
+        
+    }
+}
