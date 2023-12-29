d9eea7aae44a630bcb5c4145f87155f67caeb99d
serinaudas <serinaudas@gmail.com>
2023-12-29 20:16:24 +0500

    [4.1 Position pen(moveTo)]-Added handler function for command using overloaded two penPosition functions drawTo with PenPosition structure incanvas class

--- ASE-GPEnv-Comp1/Canvas.cs
+++ ASE-GPEnv-Comp1/Canvas.cs
@@ -7,15 +7,40 @@ using System.Threading.Tasks;
 
 namespace ASE_GPEnv_Comp1
 {
+    public struct PenPosition
+    {
+        int posX;
+        int posY;
+        public PenPosition(int x, int y)
+        {
+            posX = x;
+            posY = y;
+        }
+    }
     class Canvas
     {
         Pen pen;
         float penWidth;
         Color penColor;
+        PenPosition penPosition;
         public Canvas(Color penColor, float penWidth)
         {
             pen = new Pen(Color.Red, penWidth);
 
         }
+
+
+        public void moveTo(PenPosition  penPosition)
+        {
+            this.penPosition = penPosition;
+        }
+
+        public void moveTo(int posX, int posY)
+        {
+            this.penPosition = new PenPosition(posX, posY);
+
+        }
+
+
     }
 }
