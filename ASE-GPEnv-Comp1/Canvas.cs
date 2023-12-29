5c91536209b55cc547716874bd43b0c7e9f65ecc
serinaudas <serinaudas@gmail.com>
2023-12-29 20:05:50 +0500

    [4: Commands List]-Preparing skeleton for commands. Added Canvas Classs

---
+++ ASE-GPEnv-Comp1/Canvas.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_GPEnv_Comp1
+{
+    class Canvas
+    {
+        Pen pen;
+        float penWidth;
+        Color penColor;
+        public Canvas(Color penColor, float penWidth)
+        {
+            pen = new Pen(Color.Red, penWidth);
+
+        }
+    }
+}
