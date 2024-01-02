43c0c0b429cbe18b38683c7bc426c8dca6c496d2
serinaudas <serinaudas@gmail.com>
2024-01-03 03:13:55 +0500

    [3 Syntax Parser Class- Syntax Checkcing]- Added parameter validity check. checks for [no params, insufficient params, too much params, wrong type params

--- ASE-GPEnv-Comp1/CommandParser.cs
+++ ASE-GPEnv-Comp1/CommandParser.cs
@@ -16,26 +16,36 @@ namespace ASE_GPEnv_Comp1
     {
         public string command;
         public int numberOfValidParams;
-        public string stringParam; //for example pen {color}, only string param is valid param
+        public string[] validParams; 
         public bool hasStringParam;
- 
-
-
-        public GPLCommand(string command, int numberOfValidParams): this()
+        public string commandFormat;
+        public GPLCommand(string command) : this()
         {
             // Example commands: run; clear; reset;
             this.command = command;
-            this.numberOfValidParams = numberOfValidParams;
-            this.hasStringParam = true;
+            this.numberOfValidParams = 0;
+            this.commandFormat = command;
 
         }
-        public GPLCommand(string command, string stringParam, int numberOfValidParams) : this()
+
+
+        public GPLCommand(string command, string [] validStringParams, int numberOfValidParams, string commandFormat) : this()
         {
             //exmple commands: fill on; fill off; pen {color}
             this.command = command;
             this.numberOfValidParams = numberOfValidParams;
-            this.stringParam = stringParam;
+            this.validParams = validStringParams;
             this.hasStringParam = true;
+            this.commandFormat = commandFormat;
+        }
+
+        public GPLCommand(string command, int numberOfValidParams, string commandFormat) : this()
+        {
+            // Example commands: drawing commands;
+            this.command = command;
+            this.numberOfValidParams = numberOfValidParams;
+            this.commandFormat = commandFormat;
+
         }
     }
 
@@ -54,24 +64,24 @@ namespace ASE_GPEnv_Comp1
             this.canvas = canvas;
 
             allValidGPLCommands = new GPLCommand[] {
-                new GPLCommand("run", 0),
-                new GPLCommand("moveto", 2), //2 params: x, y
-                new GPLCommand("drawto", 2), //2 params: x, y
-                new GPLCommand("clear", 0),
-                new GPLCommand("reset", 0),
-                new GPLCommand("rectangle", 2),//2 params: width, height
-                new GPLCommand("circle", 1),//1 param: radius
-                new GPLCommand("triangle",1),//1 param: side length
+                new GPLCommand("run"),
+                new GPLCommand("moveto", 2, "moveto {x}, {y}"), //2 params: x, y
+                new GPLCommand("drawto", 2, "draw {x}, {y}"), //2 params: x, y
+                new GPLCommand("clear"),
+                new GPLCommand("reset"),
+                new GPLCommand("rectangle", 2, "rectangle {width}, {height}" ),//2 params: width, height
+                new GPLCommand("circle", 1, "draw {radius}"),//1 param: radius
+                new GPLCommand("triangle",1, "draw {side-length}"),//1 param: side length
 
-                new GPLCommand("pen", "red", 1),
-                new GPLCommand("pen", "green",1),
-                new GPLCommand("pen", "blue",1),
-                new GPLCommand("pen", "cyan",1),
-                new GPLCommand("pen", "magenta",1),
-                new GPLCommand("pen", "yellow",1),
+                new GPLCommand("pen",
+                    new string []{"red", "green", "blue", "cyan", "magenta", "yellow"},
+                    1, 
+                    "pen {color}"),
 
-                new GPLCommand("fill","on",1),
-                new GPLCommand("fill","off",1)
+                new GPLCommand("fill",
+                    new string []{"on", "off"},
+                    1, 
+                    "fill {on|off}"),
 
             };
         }
@@ -89,6 +99,22 @@ namespace ASE_GPEnv_Comp1
             }
         }
 
+        public string[] extractParamsFromCommand(string command) {  
+            //List<string> paramsArray = new List<string>();
+            string[] commandSplitBySpace = command.ToLower().Split(' ');
+            string restString = "";
+
+            int count = 0;
+            foreach (string split in commandSplitBySpace) {
+                if(count++>0)
+                    restString = restString + split;
+            }
+            restString = restString.Trim();
+
+            string[] paramsArray = restString.Split(',');
+            return restString == ""? new string[] { }: paramsArray;
+
+        }
         public ParsingInfo checkSyntax(String command, int lineNumber)
         {
             ParsingInfo parsingInfo = new ParsingInfo();
@@ -98,7 +124,7 @@ namespace ASE_GPEnv_Comp1
     
             if (command.Length == 0) {
 
-                parsingInfo.parsingExceptions.Add(new InvalidCommandException("Invalid Command","Command be an empty string"));
+                parsingInfo.parsingExceptions.Add(new InvalidCommandException("Invalid Command:","Command be an empty string"));
                 parsingInfo.isSuccessful = false;
                 return parsingInfo;
 
@@ -110,6 +136,8 @@ namespace ASE_GPEnv_Comp1
             string[] commandSplitBySpace = command.ToLower().Split(' ');
             string inputCommand = commandSplitBySpace[0];
 
+            string[] paramsArray = extractParamsFromCommand(command);
+
 
             //checking command validity here
             foreach (GPLCommand cmd_i in allValidGPLCommands) {
@@ -120,71 +148,84 @@ namespace ASE_GPEnv_Comp1
                 }
             }
             if (!hasFoundCommand) {
-                    parsingInfo.parsingExceptions.Add(new InvalidCommandException("Invalid Command", "'" + command + "' not a valid command."));
+                    parsingInfo.parsingExceptions.Add(new InvalidCommandException("Invalid Command:", "'" + command + "' not a valid command."));
             }
             // end of command validity check
 
 
 
 
-            if(false) // fix and implement in next step
             foreach (GPLCommand cmd_i in allValidGPLCommands)
             {
-                //check if string command found in paramters, otherwise add exception object
-                if (cmd_i.hasStringParam)
+                if (cmd_i.command == inputCommand)
                 {
-                    if (commandSplitBySpace.Length < 2)
+
+                    //check for COUNT of required parameters
+                    if (paramsArray.Length == 0)
                     {
-                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command Param", "Please enter complete command. Hint! Add string paramter."));
+                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command Param:", "Please enter complete command. \n\tHint! " + cmd_i.commandFormat));
+                        break;
                     }
-                    else
+
+                    else if (paramsArray.Length < cmd_i.numberOfValidParams)
                     {
+                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command Param:", "Insufficient parameters supplied. Please enter complete command. \n\tHint! " + cmd_i.commandFormat));
+                        break;
+                    }
+                    else if (paramsArray.Length > cmd_i.numberOfValidParams)
+                    {
+                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command Param:", "Too much parameters supplied. Please follow command format. \n\tHint! " + cmd_i.commandFormat));
+                        break;
+                    }
+
+                    //check if string command found in paramters, otherwise add exception object
+                    if (cmd_i.hasStringParam)
+                    {
+                        string validParamsString = "'" + string.Join("', '", cmd_i.validParams) + "'";
+
+
                         string inputStringParam = commandSplitBySpace[1];
                         bool hasFoundStringParam = false;
-                        foreach (GPLCommand cmd_j in allValidGPLCommands)
+
+                        foreach (string validStringParam in cmd_i.validParams)
                         {
-                            if (inputStringParam == cmd_j.stringParam)
+                            if (validStringParam == inputStringParam)
                             {
                                 hasFoundStringParam = true;
+                                break;
+                            }
+                        }
+
+                        if (!hasFoundStringParam)
+                        {
+                            parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command String Param:", "'" + inputStringParam + "' is not a valid param for command '" + inputCommand + "'.\n\tHint: Use any of " + validParamsString));
+
+                        }
+
+                    }
+
+                    // commands with integer one or more paramters
+                    else
+                    {
+
+                        for (int paramNumber = 0; paramNumber < cmd_i.numberOfValidParams; paramNumber++)
+                        {
+
+                            {
+                                if (!int.TryParse(paramsArray[paramNumber], out int result))
+                                {
+                                    parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Param Type", "Invalid paramter '"+paramsArray[paramNumber]+"'. Command '" + inputCommand + "' only accept integer paramters."));
+
+                                }
+
                             }
 
                         }
-                        if (!hasFoundStringParam)
-                        {
-                            parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Command String Param", "'" + inputStringParam + "' Not a valid param"));
-
-                        }
-
-
-
 
                     }
+
+
                 }
-
-
-
-                else
-                {
-                    string[] lines = command.ToLower().Split(' ');
-                    int numberOfInputParams = lines.Length;
-                    if (cmd_i.numberOfValidParams < numberOfInputParams)
-                    {
-                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Params", "Too much parameters passed."));
-
-                    }
-                    else if (cmd_i.numberOfValidParams > numberOfInputParams)
-                    {
-                        parsingInfo.parsingExceptions.Add(new InvalidParamsException("Invalid Params", "Insufficient parameters passed."));
-
-                    }
-                    else
-                    {
-                        // parameter testing passed so far. time to check paramters validity
-                    }
-                }
-
-
-
             }
 
 
@@ -211,7 +252,7 @@ namespace ASE_GPEnv_Comp1
                     }
                     catch (ParsingException pEx) {
                       
-                        string outputText = pEx.Message + "\n\t" + pEx.getParsingExceptionMessage() + "\n_______________";
+                        string outputText = pEx.Message + "\n\t" + pEx.getParsingExceptionMessage() + "\n_____________________________________________";
                         canvas.appendExecutionResultsToOutput(outputText);
                         //Debug.WriteLine(pEx.Message+" "+pEx.getParsingExceptionMessage());
 
@@ -219,10 +260,6 @@ namespace ASE_GPEnv_Comp1
                 }
                 // draw or log the exception in output panels
             }
-
-           
-
-
 
             canvas.appendCommandToHistory(commandTxt, parsingResult.isSuccessful);
 
