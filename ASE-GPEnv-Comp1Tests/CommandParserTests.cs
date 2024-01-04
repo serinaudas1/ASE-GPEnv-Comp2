f402bf2426f093c4aa5220194f29ae683f883c4f
serinaudas <serinaudas@gmail.com>
2024-01-05 01:53:14 +0500

    re-arranged test functions

--- ASE-GPEnv-Comp1Tests/CommandParserTests.cs
+++ ASE-GPEnv-Comp1Tests/CommandParserTests.cs
@@ -186,6 +186,33 @@ namespace ASE_GPEnv_Comp1.Tests
             MessageBox.Show("Test Completed");
         }
 
+
+
+        [TestMethod]
+        public void executeOneCommandTest_drawTo()
+        {
+            MainUI_AseGPL1 mainUI = new MainUI_AseGPL1();
+            mainUI.Visible = true;
+            CommandParser parser = new CommandParser(mainUI.canvas, mainUI.clearTextCB);
+            string command = "drawto 100, 100";
+
+            try
+            {
+                ParsingInfo parsingResult = parser.executeOneCommand(command, -1);
+                Assert.IsTrue(parsingResult.isSuccessful);
+            }
+            catch (InvalidCommandException ex)
+            {
+                StringAssert.Contains(ex.getParsingExceptionMessage().ToLower(), "invalid command", failedTestMessage(ex));
+                Assert.Fail(failedTestMessage(ex));
+
+            }
+
+            // added this to see the result of execution on screen
+            MessageBox.Show("Test Completed");
+        }
+
+
         [TestMethod]
         public void executeOneCommandTest_clear()
         {
@@ -220,29 +247,6 @@ namespace ASE_GPEnv_Comp1.Tests
             // added this to see the result of execution on screen
             MessageBox.Show("Test Completed");
         }
-          [TestMethod]
-        public void executeOneCommandTest_drawTo()
-        {
-            MainUI_AseGPL1 mainUI = new MainUI_AseGPL1();
-            mainUI.Visible = true;
-            CommandParser parser = new CommandParser(mainUI.canvas, mainUI.clearTextCB);
-            string command = "drawto 100, 100";
-
-            try
-            {
-                ParsingInfo parsingResult = parser.executeOneCommand(command, -1);
-                Assert.IsTrue(parsingResult.isSuccessful);
-            }
-            catch (InvalidCommandException ex)
-            {
-                StringAssert.Contains(ex.getParsingExceptionMessage().ToLower(), "invalid command", failedTestMessage(ex));
-                Assert.Fail(failedTestMessage(ex));
-
-            }
-
-            // added this to see the result of execution on screen
-            MessageBox.Show("Test Completed");
-        }
-
+        
     }
 }
